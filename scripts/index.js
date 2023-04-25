16ea190403b763cfa89612a0a1bb20fb1c73f457
Martin Lozano Muñoz <martinlozanomu@gmail.com>
2023-04-24 23:35:27 -0300

    comment cart code in index.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,30 @@
 // ------- Navbar -------
-const buttonCounter = document.getElementById("button-counter");
-const cartIconCounter = document.getElementById("cart-icon-counter");
-const modalMainCart = document.getElementById("modal-cart-main");
-const deleteCartButton = document.getElementById("delete-cart-button");
+// const buttonCounter = document.getElementById("button-counter");
+// const cartIconCounter = document.getElementById("cart-icon-counter");
+// const modalMainCart = document.getElementById("modal-cart-main");
+// const deleteCartButton = document.getElementById("delete-cart-button");
 
-let counterCart = [];
+// let counterCart = [];
 
-buttonCounter.addEventListener("click", () => {
-  counterCart++;
-  cartIconCounter.textContent = counterCart;
-  console.log(counterCart);
-});
+// buttonCounter.addEventListener("click", () => {
+//   counterCart++;
+//   cartIconCounter.textContent = counterCart;
+//   console.log(counterCart);
+// });
 
-buttonCounter.addEventListener("click", () => {
-  const div = document.createElement("div");
-  div.classList.add("product-in-cart");
-  div.innerHTML = `
-  <p>Product</p>
-  <p>Total:</p>
-  <p>Price:</p>
-  <button id="delete-cart-button" type="button" class="btn btn-danger">
-    <i class="bi bi-trash-fill"></i>
-  </button>
-  `;
-  modalMainCart.appendChild(div);
-});
+// buttonCounter.addEventListener("click", () => {
+//   const div = document.createElement("div");
+//   div.classList.add("product-in-cart");
+//   div.innerHTML = `
+//   <p>Product</p>
+//   <p>Total:</p>
+//   <p>Price:</p>
+//   <button id="delete-cart-button" type="button" class="btn btn-danger">
+//     <i class="bi bi-trash-fill"></i>
+//   </button>
+//   `;
+//   modalMainCart.appendChild(div);
+// });
 
 console.log(counterCart);
 // -------- End Navbar -------
